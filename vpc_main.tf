bada6b04c8f6b30f1763be7f9bac599bae0ca331
Anbukugan <47414816+Anbukugan@users.noreply.github.com>
2021-12-09 16:56:54 +0530

    Update vpc_main.tf

--- vpc_main.tf
+++ vpc_main.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "main" {
-  count = length(var.vpc_name)
-  cidr_block           = var.vpc_cidr[count.index]
-  instance_tenancy     = var.instance_tenancy[count.index]
-  enable_dns_support   = var.enable_dns_support[count.index]
-  enable_dns_hostnames = var.enable_dns_hostnames[count.index]
-  tags                 = merge({Name= var.vpc_name[count.index]},var.tag)
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags                 = merge({Name= var.vpc_name},var.tag)
 }
